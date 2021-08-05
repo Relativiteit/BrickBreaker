bd27e8ebd93ee5633194b42b363a963ed1f8eb05
Relativiteit <alejocain@gmail.com>
2021-08-05 12:19:25 +0200

    Added README.md

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package brickBreaker;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // create Jframe outer window of game
+        JFrame obj = new JFrame();
+        obj.setBounds(10,10, 700, 600);
+        obj.setTitle;
+    }
+}
---
+++ src/README.md
@@ -0,0 +1,4 @@
+# Brickbreaker game
+A classic brick breaking game just to play around more with java syntax and game creation. 
+## Structure 
+```Main ``` contains logic for the game loop 
